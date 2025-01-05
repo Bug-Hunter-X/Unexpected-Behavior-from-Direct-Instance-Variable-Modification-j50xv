989a9930cbd8d192386455667e94426cd4b0b390
BugHunter-X <askzenai@gmail.com>
2025-01-05 20:52:06 +0100

    Add bug.rb file

---
+++ bug.rb
@@ -0,0 +1,21 @@
+```ruby
+class MyClass
+  def initialize(value)
+    @value = value
+  end
+
+  def value
+    @value
+  end
+
+end
+
+my_object = MyClass.new(10)
+puts my_object.value # Output: 10
+
+my_object.instance_variable_set(:@value, 20) # Modifying the instance variable directly
+puts my_object.value # Output: 20
+
+#Modifying the instance variable through a setter method is the preferred approach
+# This avoids the direct modification of the instance variable. 
+```
